f30344d87f0b6d4aa399d936105b2ca669e03aa9
Pedro Rodrigues <pedronuel36@gmail.com>
2024-11-19 10:48:34 -0300

    first commit

---
+++ src/main/java/br/edu/ifce/entitys/Category.java
@@ -0,0 +1,43 @@
+package br.edu.ifce.entitys;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Category {
+    private Long id;
+    private String description;
+    private String icon;
+    private String color;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
---
+++ src/main/java/br/edu/ifce/meuprimeirospringboot/MeuprimeirospringbootApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifce.meuprimeirospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuprimeirospringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuprimeirospringbootApplication.class, args);
+	}
+
+}
